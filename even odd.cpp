f9ffc853b502184e5e1cd2a1f901e86518a87943
shivam036 <87231262+shivam036@users.noreply.github.com>
2021-09-06 19:37:17 +0530

    Update even odd.cpp

--- even odd.cpp
+++ even odd.cpp
@@ -2,17 +2,13 @@
 using namespace std;
 int main()
 {
-	int n,mod;
+	int n;
 	cout<<"enter an integer";
 	cin>>n;
-	mod=n%2==0;
-	cout<<"odd"<<(mod)<<endl;
-	return 0;}
-	
-/*	if( n % 2==0)
+	if( n % 2==0)
 	cout<<"is  even ";
 	else
 	cout<<"is odd";
 	return 0;
 }
-*/
+
